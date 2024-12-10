f64a1dc61bc7baa3d4aa5302d7d79768fcb512fd
“sobhan”912 <sobhan912@gmail.com>
2024-12-10 13:10:49 +0100

    add const strings file

---
+++ lib/constants/strings.dart
@@ -0,0 +1,21 @@
+const String textExperiment = "Text Experiment";
+const String pleaseWriteBelow = "Please write below";
+const String btnRefresh = "Refresh";
+const String btnDataCollection = "Data Collection";
+const String btnExport = "Export PDF";
+const String dataCollectionPageTitle = "Data Collection : Using stylus";
+const String btnConnection = "Connected";
+const String adminDashboardPageTitle = "Data Collection and Testing Tool";
+const List<String> randomWords = [
+  "Lorem",
+  "Nuwa",
+  "magic pen",
+  "Flutter",
+  "Technology",
+  "Netherlands",
+  "Europe",
+  "Code",
+  "Assignment",
+  "Sam",
+  "Company"
+];
